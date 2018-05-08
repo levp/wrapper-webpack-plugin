d12a4b8ddbeb3697da85a95bb5e0f6a2d9f297b1
levp <levi2p@gmail.com>
2018-05-08 12:47:26 +0300

    Fixes #4 - Switched to using Tappable. Upgraded to ES6. Extra arguments are now provided to the `header` and `footer` if they are functions.

--- wrapper-webpack-plugin.js
+++ wrapper-webpack-plugin.js
@@ -1,61 +1,55 @@
 'use strict';
 
-var ConcatSource = require("webpack-sources").ConcatSource;
-var ModuleFilenameHelpers = require("webpack/lib/ModuleFilenameHelpers");
+const ConcatSource = require("webpack-sources").ConcatSource;
+const ModuleFilenameHelpers = require("webpack/lib/ModuleFilenameHelpers");
 
-/**
- * @param args
- * @param {string|function} [args.header]
- * @param {string|function} [args.footer]
- * @param {string|RegExp} [args.test]
- * @constructor
- */
-function WrapperPlugin(args) {
-	if (typeof args !== 'object') {
-		throw new TypeError('Argument "args" must be an object.');
+class WrapperPlugin {
+	constructor(args) {
+		if (typeof args !== 'object') {
+			throw new TypeError('Argument "args" must be an object.');
+		}
+
+		this.header = args.hasOwnProperty('header') ? args.header : '';
+		this.footer = args.hasOwnProperty('footer') ? args.footer : '';
+		this.test = args.hasOwnProperty('test') ? args.test : '';
 	}
 
-	this.header = args.hasOwnProperty('header') ? args.header : '';
-	this.footer = args.hasOwnProperty('footer') ? args.footer : '';
-	this.test = args.hasOwnProperty('test') ? args.test : '';
-}
+	apply(compiler) {
+		const header = this.header;
+		const footer = this.footer;
+		const tester = {test: this.test};
 
-function apply(compiler) {
-	var header = this.header;
-	var footer = this.footer;
-	var tester = {test: this.test};
-
-	compiler.plugin('compilation', function (compilation) {
-		compilation.plugin('optimize-chunk-assets', function (chunks, done) {
-			wrapChunks(compilation, chunks, footer, header);
-			done();
-		})
-	});
-
-	function wrapFile(compilation, fileName) {
-		var headerContent = (typeof header === 'function') ? header(fileName) : header;
-		var footerContent = (typeof footer === 'function') ? footer(fileName) : footer;
-
-		compilation.assets[fileName] = new ConcatSource(
-				String(headerContent),
-				compilation.assets[fileName],
-				String(footerContent));
-	}
-
-	function wrapChunks(compilation, chunks) {
-		chunks.forEach(function (chunk) {
-			chunk.files.forEach(function (fileName) {
-				if (ModuleFilenameHelpers.matchObject(tester, fileName)) {
-					wrapFile(compilation, fileName);
-				}
-			});
+		compiler.hooks.compilation.tap('WrapperPlugin', (compilation) => {
+			compilation.hooks.optimizeChunkAssets.tapAsync('WrapperPlugin', (chunks, done) => {
+				wrapChunks(compilation, chunks, footer, header);
+				done();
+			})
 		});
+
+		function wrapFile(compilation, fileName, chunkHash) {
+			const headerContent = (typeof header === 'function') ? header(fileName, chunkHash) : header;
+			const footerContent = (typeof footer === 'function') ? footer(fileName, chunkHash) : footer;
+
+			compilation.assets[fileName] = new ConcatSource(
+					String(headerContent),
+					compilation.assets[fileName],
+					String(footerContent));
+		}
+
+		function wrapChunks(compilation, chunks) {
+			chunks.forEach(chunk => {
+				const args = {
+					hash: compilation.hash,
+					chunkhash: chunk.hash
+				};
+				chunk.files.forEach(fileName => {
+					if (ModuleFilenameHelpers.matchObject(tester, fileName)) {
+						wrapFile(compilation, fileName, args);
+					}
+				});
+			});
+		}
 	}
 }
-
-Object.defineProperty(WrapperPlugin.prototype, 'apply', {
-	value: apply,
-	enumerable: false
-});
 
 module.exports = WrapperPlugin;
